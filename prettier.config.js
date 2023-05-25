c113a43e2584c5d9c9a17683a9aa17913299b2b4
stephan-anthony <132624765+stephan-anthony@users.noreply.github.com>
2023-05-25 00:19:34 -0300

    Add files via upload

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  semi: true,
+  requirePragma: false,
+  proseWrap: "preserve",
+  arrowParens: "avoid",
+};
